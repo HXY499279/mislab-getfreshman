1da423007058c59239944b35ddab677caf8dac36
HXY <1969574105@qq.com>
2021-07-29 17:36:31 +0800

    第一页动画全部完成

---
+++ .history/src/components/Home/Invatation/index_20210729161741.jsx
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react'
+import {  Link } from 'react-router-dom';
+import { Button, Image } from 'antd';
+// 引入外部资源
+import logo from '../../../assets/logo.png'
+import './index.css'
+import '../../../static/iconfont.css'
+import "antd/dist/antd.css"
+
+
+export default function index() {
+    return (
+        <Fragment>
+            <div className="mislabVideoWrap">
+                <video src={"http://qwu8f72zr.hn-bkt.clouddn.com/shouyeshipin.mp4"} muted autoPlay loop className="mislabVideo" />
+                <div className="cloth" />
+                <div className="nav">
+                    <div className="nav-left">
+                        <Link to='/'>
+                            <Image
+                                src={logo}
+                                preview={false}
+                                className="nav-left-logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="nav-right">
+                        <div
+                            className="nav-right-a"
+                        >
+                            <a herf="">关于我们</a>
+                            <a herf="">产品矩阵</a>
+                            <a herf=""></a>
+                            <a herf="">成员去向</a>
+                            <a herf="">招新流程</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="mainTitle-wrap">
+                    <div className="mainTitle">
+                        <div>信管工作室&nbsp;·&nbsp;2021年度邀请函</div>
+                        <div>想和优秀的你&nbsp;&nbsp;&nbsp;&nbsp;一起改变世界</div>
+                        <div className="mainTitle-subtitle">
+                            JOIN MISLAB
+                        </div>
+                        <Button size="large" className="mainTitle-button">
+                            <span className="mainTitle-button-words">加入我们</span>
+                        </Button>
+                    </div>
+                </div>
+                <div className="sub-Icon">
+                    <span className="sub-Icon-iconfont">
+                        <span className="iconfont">&#xe656;</span>
+                    </span>
+                </div>
+                <div className="sub-xuxian" />
+            </div>
+
+        </Fragment>
+    )
+}
---
+++ .history/src/components/Home/Invatation/index_20210729172839.jsx
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react'
+import {  Link } from 'react-router-dom';
+import { Button, Image } from 'antd';
+// 引入外部资源
+import logo from '../../../assets/logo.png'
+import './index.css'
+import '../../../static/iconfont.css'
+import "antd/dist/antd.css"
+
+
+export default function index(funs) {
+    const {switchPage} = funs.funs.switchPage
+    return (
+        <Fragment>
+            <div className="mislabVideoWrap">
+                <video src={"http://qwu8f72zr.hn-bkt.clouddn.com/shouyeshipin.mp4"} muted autoPlay loop className="mislabVideo" />
+                <div className="cloth" />
+                <div className="nav">
+                    <div className="nav-left">
+                        <Link to='/'>
+                            <Image
+                                src={logo}
+                                preview={false}
+                                className="nav-left-logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="nav-right">
+                        <div
+                            className="nav-right-a"
+                        >
+                            <a href="#page1">关于我们</a>
+                            <a href="#page2">部门介绍</a>
+                            <a href="#page3">产品介绍</a>
+                            <a href="#page4">成员动态</a>
+                            <a href="#page5">招新流程</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="mainTitle-wrap">
+                    <div className="mainTitle">
+                        <div>信管工作室&nbsp;·&nbsp;2021年度邀请函</div>
+                        <div>想和优秀的你&nbsp;&nbsp;&nbsp;&nbsp;一起改变世界</div>
+                        <div className="mainTitle-subtitle">
+                            JOIN MISLAB
+                        </div>
+                        <Button size="large" className="mainTitle-button">
+                            <span className="mainTitle-button-words" onc>加入我们</span>
+                        </Button>
+                    </div>
+                </div>
+                <div className="sub-Icon">
+                    <span className="sub-Icon-iconfont">
+                        <span className="iconfont">&#xe656;</span>
+                    </span>
+                </div>
+                <div className="sub-xuxian" />
+            </div>
+
+        </Fragment>
+    )
+}
