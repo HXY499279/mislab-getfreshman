42036e17ca4dbe29b141e3078e4ef730c955115d
HXY <1969574105@qq.com>
2021-08-01 16:56:57 +0800

    优化了手机端的横屏提示为两个的问题，优化了视频在手机端加载不出来的问题

---
+++ .history/src/components/Home/Invatation/index_20210801161408.jsx
@@ -0,0 +1,78 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import { Button, Image } from 'antd';
+// 引入外部资源
+import logo from '../../../assets/logo.png'
+import bgpic from '../../../assets/bgpic.jpg'
+import './index.css'
+import '../../../static/iconfont.css'
+import "antd/dist/antd.css"
+
+
+export default function index(funs) {
+    const { switchPage } = funs.funs
+    return (
+        <Fragment>
+            <div className="mislabVideoWrap">
+                <video
+                    src={"http://qwu8f72zr.hn-bkt.clouddn.com/shouyeshipin.mp4"}
+                    muted
+                    preload="auto"
+                    playsinline
+                    tabIndex="-1"
+                    autoPlay
+                    loop
+                    className="mislabVideo"
+                    webkit-playsinline="true" playsinline="true"   x5-playsinline="" 
+                    x-webkit-airplay="allow"
+                     x5-video-player-type="h5" 
+                     x5-video-player-fullscreen="true" 
+                     x5-video-orientation="portraint"
+
+                />
+                <div className="cloth" />
+                <div className="nav">
+                    <div className="nav-left">
+                        <Link to='/'>
+                            <Image
+                                src={logo}
+                                preview={false}
+                                className="nav-left-logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="nav-right">
+                        <div
+                            className="nav-right-a"
+                        >
+                            <a href="#page1">关于我们</a>
+                            <a href="#page2">部门介绍</a>
+                            <a href="#page3">产品介绍</a>
+                            <a href="#page4">成员动态</a>
+                            <a href="#page5">招新流程</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="mainTitle-wrap">
+                    <div className="mainTitle">
+                        <div>信管工作室&nbsp;·&nbsp;2021年度邀请函</div>
+                        <div>想和优秀的你&nbsp;&nbsp;&nbsp;&nbsp;一起改变世界</div>
+                        <div className="mainTitle-subtitle">
+                            JOIN MISLAB
+                        </div>
+                        <Button size="large" className="mainTitle-button">
+                            <a href="#page5"><span className="mainTitle-button-words">加入我们</span></a>
+                        </Button>
+                    </div>
+                </div>
+                <div className="sub-Icon" onClick={switchPage.bind(this, 810)}>
+                    <span className="sub-Icon-iconfont">
+                        <span className="iconfont">&#xe656;</span>
+                    </span>
+                </div>
+                <div className="sub-xuxian" />
+            </div>
+
+        </Fragment>
+    )
+}
