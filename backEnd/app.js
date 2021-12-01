5459edb747da96b744ea1baf8a67bf8a117e7a17
HXY <1969574105@qq.com>
2021-12-01 10:35:12 +0800

    👑：去掉控制台的打印

--- backEnd/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, '../build')));
-
-app.listen(9000);
-module.exports = app
